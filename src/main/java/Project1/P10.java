4b07de689b138db22d24b6334d81b5737b6c78bf
babybluetaro <nathan.8993.p@gmail.com>
2024-03-18 10:13:17 -0700

    first commit

--- src/main/java/Project1/P10.java
+++ src/main/java/Project1/P10.java
@@ -5,7 +5,7 @@ public class P10 {
         String[] words={"AA","BB","CC","AA","AC","CC"};
         for (int i=0;i< words.length;i++)
             for (int j=i+1;j< words.length;j++){
-                if (words[i]==words[j]){
+                if (words[i].equals(words[j])){
                     System.out.println(words[i]);
                 }
             }
